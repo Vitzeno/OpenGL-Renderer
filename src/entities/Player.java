d85a81c97c46a3496032197c0821dca494b60777
Mohamed Yusuf <vitzeno@gmail.com>
2018-08-09 15:20:01 +0100

    Implement basic player jumping and gravity

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -15,9 +15,16 @@ public class Player extends Entity{
 	
 	private static final float RUN_SPEED = 20;
 	private static final float TURN_SPEED = 160;
+	private static final float GRAVITY = -50;
+	private static final float JUMP_FORCE = 30;
+	
+	private static final float TERRAIN_HEIGHT = 0;
 	
 	private float currentSpeed = 0;
 	private float currentTurnSpeed = 0;
+	private float upwardsSpeed = 0;
+	
+	private boolean isInAir = false;
 
 	public Player(TexturedModel model, Vector3f position, float rotX, float rotY, float rotZ, float scale) {
 		super(model, position, rotX, rotY, rotZ, scale);
@@ -33,10 +40,30 @@ public class Player extends Entity{
 		float dx = (float) (distance * Math.sin(Math.toRadians(super.getRotY())));
 		float dz = (float) (distance * Math.cos(Math.toRadians(super.getRotY())));
 		super.increasePosition(dx, 0, dz);
+		
+		upwardsSpeed += GRAVITY * DisplayManager.getFrameTimeSeconds();
+		super.increasePosition(0, upwardsSpeed * DisplayManager.getFrameTimeSeconds(), 0);
+		
+		if(super.getPosition().y < TERRAIN_HEIGHT) {
+			upwardsSpeed = 0;
+			isInAir = false;
+			super.getPosition().y = TERRAIN_HEIGHT;
+		}
 	}
 	
 	/**
-	 * 
+	 * This method handled player jumping
+	 */
+	private void jump() {
+		if(!isInAir) {
+			upwardsSpeed = JUMP_FORCE;
+			isInAir = true;
+		}
+		
+	}
+	
+	/**
+	 * Simple method for checking keyboard inputs
 	 */
 	public void checkInputs() {
 		if(Keyboard.isKeyDown(Keyboard.KEY_W)){
@@ -54,6 +81,10 @@ public class Player extends Entity{
         }else {
         	currentTurnSpeed = 0;
         }
+		
+		if(Keyboard.isKeyDown(Keyboard.KEY_SPACE)) {
+			jump();
+		}
 		
 	}
 }
